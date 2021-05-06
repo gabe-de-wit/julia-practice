070e8264314a88d14a272e1b2a18e07ee26be636
gabe-de-wit <58444672+gabe-de-wit@users.noreply.github.com>
2021-05-06 08:44:46 +0100

    added borough populations

--- src/2_analyze_data.jl
+++ src/2_analyze_data.jl
@@ -2,6 +2,7 @@ using Pipe
 using DataFrames
 using CSV
 using Dates
+using Plots
 
 ## ----------------- Import
 
@@ -12,6 +13,7 @@ using Dates
 
 victims_import = DataFrame(CSV.File("data/homicide_victims.csv"))
 ppa_import = DataFrame(CSV.File("data/homicide_ppa.csv"))
+borough_population_import = DataFrame(CSV.File("data/borough_population.csv"))
 
 ## ----------------- Wrangle
 
@@ -26,14 +28,28 @@ victims = @pipe victims_import |>
         :recorded_date => (x -> Date.(x, "u Y")) => :recorded_date,
         :sex =>               (x -> replace.(x, "Unrecorded" => missing))             => :sex,
         :method_of_killing => (x -> replace.(x, "Not known/Not Recorded" => missing)) => :method_of_killing
+    ) |>
+    transform(
+        _,
+        :recorded_date => (x -> year.(x)) => :recorded_year,
     )
 
 ppa = @pipe ppa_import |>
     rename(clean_names, _) |>
     transform(
         _,
-        :proceedings_date => (x -> Date.(replace.(x, "Febraury" => "February"), "U Y")) => :proceedings_date # 'February' was misspelled on one line
+        :proceedings_date => (x -> Date.(replace.(x, "Febraury" => "February"), "U Y")) => :proceedings_date, # 'February' was misspelled on one line
+    ) |>
+    transform(
+        _,
+        :proceedings_date => (x -> year.(x)) => :proceedings_year,
     )
+
+borough_population = @pipe borough_population_import |>
+    rename(clean_names, _) |>
+    rename(_, :name => :borough_name) |>
+    select(_, [:borough_name, :year, :population]) |>
+    filter(:year => (x -> (x >= 2003 && x <= 2020)), _)
 
 ## ----------------- Miscellaneous Exploratory Summary Tables
 
@@ -51,3 +67,17 @@ method_gender_split = @pipe victims |>
     combine(_, :count_of_victims => sum => :n_victims) |>
     groupby(_, :sex) |>
     transform(_, :n_victims => (a -> a ./ sum(a)) => :pct_victims)
+
+## ----------------- Calculate annual homicide rates
+
+borough_annual_homicides = @pipe victims |>
+    groupby(_, [:borough, :recorded_year]) |>
+    combine(_, :count_of_victims => sum => :n_victims) |>
+    leftjoin(_, borough_population, on = [:recorded_year => :year, :borough => :borough_name]) |>
+    transform(_, [:n_victims, :population] => ((x, y) -> x ./ y * 100000) => :homicide_rate_per_100k)
+
+## ----------------- Export
+
+if isdir("output") return else mkdir("output") end
+
+borough_annual_homicides |> CSV.write("output/borough_annual_homicides.csv")
